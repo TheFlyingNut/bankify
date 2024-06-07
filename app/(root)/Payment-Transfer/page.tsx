01549556917b160e8b6a9a13366865c1f8c64a4d
Kartik <kartiktornadus098@gmail.com>
2024-06-07 13:14:42 +0530

    second commit

---
+++ app/(root)/Payment-Transfer/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PaymentTransfer
+ = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PaymentTransfer
+
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,38 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Kartik'};
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1024.34}
+          />
+        </header>
+
+        {/* <RecentTransactions 
+          accounts={accountsData}
+          transactions={account?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        />*/}
+      </div> 
+
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{currentBalance: 123.56}, {currentBalance: 500}]}/>
+    </section>
   )
 }
 
